70120dc6ff93a41960ab38c9fe2a2828ff2abca9
ktk8916 <ktk8916@naver.com>
2023-11-01 14:32:54 +0900

    init

---
+++ src/main/java/com/playdata/taskquery/TaskQueryApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.taskquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskQueryApplication.class, args);
+    }
+
+}
