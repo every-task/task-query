069ab0830996aaabc539a88a96b7ba5c21abed79
ktk8916 <ktk8916@naver.com>
2023-11-03 10:14:29 +0900

    feat: 성공담 태스크 조회 JPQL로 변경

--- src/main/java/com/playdata/taskquery/domain/task/repository/TaskRepository.java
+++ src/main/java/com/playdata/taskquery/domain/task/repository/TaskRepository.java
@@ -2,11 +2,13 @@ package com.playdata.taskquery.domain.task.repository;
 
 import com.playdata.taskquery.domain.task.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 
-    List<Task> findByStoryId(Long id);
+    @Query("select t from Task t where t.StoryId = :storyId")
+    List<Task> findByStoryId(Long storyId);
 }
