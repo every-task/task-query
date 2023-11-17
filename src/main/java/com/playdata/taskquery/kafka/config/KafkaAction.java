bdb0cbe4098c9072c28cee326d51c83fbeb7a536
ktk8916 <ktk8916@naver.com>
2023-11-17 15:15:04 +0900

    feat: kafka action 추가

---
+++ src/main/java/com/playdata/taskquery/kafka/config/KafkaAction.java
@@ -0,0 +1,7 @@
+package com.playdata.taskquery.kafka.config;
+
+public enum KafkaAction {
+    CREATE,
+    UPDATE,
+    DELETE
+}
--- src/main/java/com/playdata/taskquery/kafka/consumer/StoryConsumer.java
+++ src/main/java/com/playdata/taskquery/kafka/consumer/StoryConsumer.java
@@ -1,5 +1,7 @@
 package com.playdata.taskquery.kafka.consumer;
 
+import com.playdata.taskquery.kafka.config.KafkaAction;
+import com.playdata.taskquery.kafka.config.KafkaData;
 import com.playdata.taskquery.kafka.config.TopicConfig;
 import com.playdata.taskquery.kafka.data.StoryKafkaData;
 import com.playdata.taskquery.service.TaskService;
@@ -13,8 +15,17 @@ public class StoryConsumer {
 
     private final TaskService taskService;
 
+    // TODO : 더 좋은 action 구분 방법..
+    // @Controller, @GepMapping 처럼
+    // 만들어봐도 재밌을듯
     @KafkaListener(topics = TopicConfig.STORY)
-    public void listen(StoryKafkaData data){
+    public void listen(KafkaData<StoryKafkaData> kafkaData){
+        if(kafkaData.action() == KafkaAction.CREATE){
+            handleCreate(kafkaData.data());
+        }
+    }
+
+    public void handleCreate(StoryKafkaData data){
         taskService.taskRegister(data);
     }
 }
